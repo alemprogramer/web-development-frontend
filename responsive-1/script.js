116732d57f223c317bb4e70276fee33825025d01
lm-hossain <aleam.hossain@mymdoc.com>
2024-08-07 12:43:53 +0600

    animation finish

--- responsive-1/script.js
+++ responsive-1/script.js
@@ -13,6 +13,10 @@ function circleMouseFollower(){
 
 circleMouseFollower();
 
+function imageLocationDetect(){
+
+}
+
 
 
 gsap.from('nav',{
@@ -38,5 +42,32 @@ tl.from('.section-three',{
     y: -10,
     duration:.2,
     opacity: 0
+})
+
+
+
+document.querySelectorAll('.elem').forEach((elem)=>{
+    let diffRote = 0;
+    let rotated = 0;
+    elem.addEventListener('mouseleave',(data)=>{
+        
+        gsap.to(elem.querySelector('img'),{
+         opacity: 0,
+         ease:Power1,
+        } )
+     })
+
+    elem.addEventListener('mousemove',(data)=>{
+       let diff = data.clientY - elem.getBoundingClientRect().top;
+       diffRote  = rotated - data.clientX ;
+       rotated = data.clientX
+       gsap.to(elem.querySelector('img'),{
+        opacity: 1,
+        ease:Power3,
+        top: diff,
+        left: data.clientX,
+        rotate: gsap.utils.clamp(-20,20,diffRote)
+       } )
+    })
 })
 
